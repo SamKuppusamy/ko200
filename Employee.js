60eb9db199286fde8c7cbcd539b342c96b625d18
Sam.Kuppusamy <sambanthamk@hotmail.com>
2017-04-16 17:15:37 -0500

    Add files via upload

---
+++ Employee.js
@@ -0,0 +1,11 @@
+﻿var employee = function (first, middle, last, address) {
+    this.firstName = ko.observable(first);
+    this.middleName = ko.observable(middle);
+    this.lastName = ko.observable(last);
+    this.address = ko.observable(address);
+
+    this.fullAddress = ko.computed(function () {
+        return this.firstName() + " " + this.middleName()  + " " +this.lastName() + this.address();
+    }, this);
+};
+ko.applyBindings(new employee("Ravi", "Verma", "Perumal", "830 Anna Nagar, Trivandrum 511022"));
